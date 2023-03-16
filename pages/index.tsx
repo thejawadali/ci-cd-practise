2009a957c321e69ee4ada0d75cf25bbe1580a738
Jawad Ali <jawad.ali@repairdesk.co>
2023-03-16 21:31:12 +0500

    write unit test for home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,24 @@
-import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+
 import Counter from "../components/Counter";
 
-const Home: NextPage = () => {
+export default function Home() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <Counter></Counter>
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="w-full h-screen flex flex-col items-center justify-center">
+        <h1>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+        <div className="mt-10 flex justify-center items-center">
+          <Counter></Counter>
+        </div>
+      </main>
     </div>
   );
-};
-
-export default Home;
+}
